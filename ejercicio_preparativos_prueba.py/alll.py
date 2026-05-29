3dcbf222b4b11677c97282506c191aecc773d901
Your Name <you@example.com>
2026-05-29 16:52:02 -0400

    comenzando ej.patrte1.

---
+++ ejercicio_preparativos_prueba.py/alll.py
@@ -0,0 +1,20 @@
+dicc={
+    "rut": "123-0",
+    "nombre": "Pedro"
+ }
+print(dicc["rut"])
+
+listas=[1,2,True, "Juan"]
+    #   0 1  2      3
+print(listas[3])
+
+
+#ejemplo
+
+"""productos = {
+"Mouse": [10, 15000],
+"Teclado": [5, 25000],
+"Monitor": [3, 180000]
+} """
+
+#print(productos["Mouse"][1])
